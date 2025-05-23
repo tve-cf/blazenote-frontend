460be697caa9599e42b8cdd3e073f1ef5978c811
shahrul <shahrul@cloudflare.com>
2025-05-23 13:17:33 +0800

    Fix upload error

--- src/components/ui/AttachmentList.tsx
+++ src/components/ui/AttachmentList.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Image, Paperclip } from "lucide-react";
 import { Attachment } from "../../types";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 interface AttachmentListProps {
   noteId: string;
